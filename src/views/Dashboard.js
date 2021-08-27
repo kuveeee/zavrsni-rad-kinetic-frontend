52719269d54730ca5624da992e14134e0ac1b8f1
Domagoj <domagojkuvezdic@gmail.com>
2021-08-27 11:16:39 +0200

    Animations!
    Animation when hovering widgets on dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -4,57 +4,58 @@ import '../assets/styles/Dashboard.css';
 
 function Dashboard() {
   return <div className="main">
-    <div class="flex_row">
-      {/* <Sidebar /> */}
-      <div class="flex_column">
-        <div class="dashboard_body">
-          <div class="dashboard_h1">
-            <h1>Nadzorna ploča</h1>
-          </div>
-          <div class="break"></div>
-          <Link exact to="/Kalendar">
-            <div class="kalendar" >
-              <p>Kalendar</p>
+    <div class="dashboard_body">
+      <div class="flex_row">
+        {/* <Sidebar /> */}
+        <div class="flex_column">
+          <div class="dashboard_body">
+            <div class="dashboard_h1">
+              <h1>Nadzorna ploča</h1>
             </div>
-          </Link>
-          <Link exact to="/Nova-Rezervacija">
-            <div class="nova_rezervacija" >
-              <p>Nova rezervacija</p>
-            </div>
-          </Link>
-          <div class="termini">
-            <p></p>
-          </div>
-          <div class="break"></div>
-          <Link exact to="/Klijenti">
-            <div class="klijenti" >
-              <p>Klijenti</p>
-              <div class="break"></div>
-              <p>250</p>
-            </div>
-          </Link>
-          <div class="financije" >
-            <p>Financije</p>
-          </div>
-          <div class="break"></div>
-          <Link exact to="/Usluge">
-            <div class="usluge" >
-              <p>Usluge</p>
-              <div class="tablica">
-                <ul>
-                  <ol>
-                    <h3>Naziv usluge</h3>
-                  </ol>
-                </ul>
+            <div class="break"></div>
+            <Link exact to="/Kalendar">
+              <div class="kalendar" >
+                <p>Kalendar</p>
               </div>
+            </Link>
+            <Link exact to="/Nova-Rezervacija">
+              <div class="nova_rezervacija" >
+                <p>Nova rezervacija</p>
+              </div>
+            </Link>
+            <div class="termini">
+              <p></p>
             </div>
-          </Link>
-          <Link exact to="/Naplaćivanje">
-            <div class="naplacivanje" >
-              <p>Naplaćivanje</p>
+            <div class="break"></div>
+            <Link exact to="/Klijenti">
+              <div class="klijenti" >
+                <p>Klijenti</p>
+                <div class="break"></div>
+                <p>250</p>
+              </div>
+            </Link>
+            <div class="financije" >
+              <p>Financije</p>
             </div>
-          </Link>
-          {/* <div class="termini">
+            <div class="break"></div>
+            <Link exact to="/Usluge">
+              <div class="usluge" >
+                <p>Usluge</p>
+                <div class="tablica">
+                  <ul>
+                    <ol>
+                      <h3>Naziv usluge</h3>
+                    </ol>
+                  </ul>
+                </div>
+              </div>
+            </Link>
+            <Link exact to="/Naplaćivanje">
+              <div class="naplacivanje" >
+                <p>Naplaćivanje</p>
+              </div>
+            </Link>
+            {/* <div class="termini">
                     <p>Slijedeći termini</p>
                     <div class="termin">
                         <p><strong>Lorem ipsum</strong> sa Saeed Kay</p>
@@ -87,6 +88,7 @@ function Dashboard() {
                         <p>08:00-09:00</p>
                     </div>
                 </div> */}
+          </div>
         </div>
       </div>
     </div>
