78110261b0fb05cec94bc7cf7ee2d5459b621c6f
Domagoj <domagojkuvezdic@gmail.com>
2021-09-23 03:10:40 +0200

    getting services from db

--- src/views/Usluge.js
+++ src/views/Usluge.js
@@ -1,7 +1,6 @@
-import React, { useEffect }  from 'react';
+import React, { useEffect } from 'react';
 import '../assets/styles/Usluge.css';
 import { Link } from 'react-router-dom';
-import UslugaPojedinacno from '../components/uslugaPojedinacno';
 import plus from '../assets/images/usluge/plus.png';
 import { Services } from '../services/index';
 // import trajanje from '../assets/images/usluge/trajanje.png';
@@ -19,41 +18,62 @@ function Usluge() {
     getServices();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <div className="main">
-    <div class="flex_row">
-      <div class="flex_column">
-        <div class="body">
-          <div class="h1_usluge">
-            <h1>Usluge</h1>
-          </div>
-          <Link style={{ textDecoration: "none" }} exact to="/nova-usluga">
-            <div class="nova_usluga">
-              <img src={plus} alt="Dodavanje usluge"></img>
-              <h3>Nova usluga</h3>
+  return <div>
+    <div className="main">
+      <div class="flex_row">
+        <div class="flex_column">
+          <div class="body">
+            <div class="h1_usluge">
+              <h1>Usluge</h1>
             </div>
-          </Link>
-          <div class="flex_row">
-            <div class="usluge_tablica">
-              <li>NAZIV USLUGE</li>
-              <li>TRAJANJE</li>
-              <li>CIJENA</li>
+            <Link style={{ textDecoration: "none" }} exact to="/nova-usluga">
+              <div class="nova_usluga">
+                <img src={plus} alt="Dodavanje usluge"></img>
+                <h3>Nova usluga</h3>
+              </div>
+            </Link>
+            <div class="flex_row">
+              <div class="usluge_tablica">
+                <li>NAZIV USLUGE</li>
+                <li>TRAJANJE</li>
+                <li>CIJENA</li>
+                <li>OPIS</li>
+              </div>
+            </div>
+            <div>
+              {services.map((service) => (
+                <div class="flex_row">
+                  <div class="rectangle">
+                    <div class="flex_column">
+                      <div class="usluga">
+                        <p>{service.service_name}</p>
+                      </div>
+                    </div>
+                    <div class="flex_column">
+                      <div class="trajanje">
+                        <p>Trajanje:   <strong>{service.service_duration}</strong></p>
+                      </div>
+                    </div>
+                    <div class="flex_column">
+                      <div class="cijena">
+                        <p>Cijena:   <strong>{service.service_price}</strong></p>
+                      </div>
+                    </div>
+                    <div class="flex_column">
+                      <div class="opis">
+                        <p>{service.service_description}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+              }
             </div>
           </div>
-          <UslugaPojedinacno />
-          <div class="break"></div>
-          <UslugaPojedinacno />
-          <div class="break"></div>
-          <UslugaPojedinacno />
-          <div class="break"></div>
-          <UslugaPojedinacno />
-          <div class="break"></div>
-          <UslugaPojedinacno />
-          <div class="break"></div>
-          <UslugaPojedinacno />
         </div>
       </div>
-    </div>
-  </div>;
+    </div>;
+  </div>
 }
 
 export default Usluge;
