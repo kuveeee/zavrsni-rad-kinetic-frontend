fd7ec363c5823cbed70fcd96a09070f96c292fde
Domagoj <domagojkuvezdic@gmail.com>
2021-09-24 08:32:59 +0200

    Show clients and services in a dropdown

--- src/views/Naplacivanje.js
+++ src/views/Naplacivanje.js
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/styles/Naplacivanje.css';
+import { Clients, Services } from '../services';
 
 function Naplacivanje() {
+    const [services, setServices] = React.useState([]);
+    const [clients, setClients] = React.useState([]);
+
+    const getClients = async () => {
+        let res = await Clients.getAllClients();
+        setClients(res);
+        console.log(clients);
+    };
+
+    const getServices = async () => {
+        let res = await Services.getAllServices();
+        setServices(res);
+        console.log(services);
+    };
+    useEffect(() => {
+        getServices();
+        getClients();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const [data, setData] = useState({
+        startdate: '',
+        enddate: '',
+        title: '',
+        serviceid: '',
+        clientid: '',
+    });
+
+    function handle(e) {
+        const newdata = { ...data };
+        newdata[e.target.id] = e.target.value;
+        setData(newdata);
+        console.log(newdata);
+    }
+
     return <div className="main">
         <div class="naplacivanje_body">
             <h1>Naplaćivanje</h1>
             <div class="break"></div>
             <h6>Pacijent</h6>
             <div class="break"></div>
-            <select class="ui dropdown">
-                <option value="">Pacijent</option>
-                <option value="1">Saaed Kay</option>
-                <option value="0">Saaed Kay</option>
-                <option value="2">Saaed Kay</option>
-                <option value="3">Saaed Kay</option>
-                <option value="4">Saaed Kay</option>
-                <option value="5">Saaed Kay</option>
-                <option value="6">Saaed Kay</option>
-                <option value="7">Saaed Kay</option>
-                <option value="8">Saaed Kay</option>
+            <select onChange={(e) => handle(e)} id="clientid" value={data.clientid} class="ui_dropdown">
+                {clients.map((client) => (
+                    <option value={client.client_id}>
+                        {client.client_first_name + ' ' + client.client_last_name}
+                    </option>
+                ))}
             </select>
             <div class="break"></div>
             <h6>Dijagnoza</h6>
@@ -38,24 +69,22 @@ function Naplacivanje() {
             <div class="break"></div>
             <h6>Usluga</h6>
             <div class="break"></div>
-            <select class="ui dropdown">
-                <option value="">Usluga</option>
-                <option value="1">Usluga</option>
-                <option value="0">Usluga</option>
-                <option value="2">Usluga</option>
-                <option value="3">Usluga</option>
-                <option value="4">Usluga</option>
-                <option value="5">Usluga</option>
-                <option value="6">Usluga</option>
-                <option value="7">Usluga</option>
-                <option value="8">Usluga</option>
+            <select
+                onChange={(e) => handle(e)}
+                id="serviceid"
+                value={data.serviceid}
+                class="ui_dropdown"
+            >
+                {services.map((service) => (
+                    <option value={service.service_id}>{service.service_name}</option>
+                ))}
             </select>
             <div class="break"></div>
             <h6>Cijena</h6>
             <div class="break"></div>
             <input type="text"></input>
             <div class="kreiraj_naplacivanje">
-              <button>Kreiraj</button>
+                <button>Kreiraj</button>
             </div>
         </div>
     </div>
